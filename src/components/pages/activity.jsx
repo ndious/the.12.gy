c57f7f3bfdb601bdeda586192c1a8d82e027c922
Nicolas Dufreche <n.dufreche@gmail.com>
2017-04-05 12:12:38 +0200

    update graph activity

--- src/components/pages/activity.jsx
+++ src/components/pages/activity.jsx
@@ -9,7 +9,7 @@ const Activity = () => (
       </Col>
       <Col xs={12} md={8} mdOffset={2}>
         <ResponsiveEmbed a4by3>
-          <figure><embed src="https://wakatime.com/share/@ndufreche/1f4befc2-85f2-4f68-8f0a-1924530be931.svg"></embed></figure>
+          <figure><embed src="https://wakatime.com/share/@ndufreche/8baf5962-4bf0-406f-bbd8-4a126c31cad5.svg"></embed></figure>
         </ResponsiveEmbed>
       </Col>
       <Col xs={12} md={8} mdOffset={2}>
@@ -17,7 +17,7 @@ const Activity = () => (
       </Col>
       <Col xs={12} md={8} mdOffset={2}>
         <ResponsiveEmbed a4by3>
-          <embed src="https://wakatime.com/share/@ndufreche/9539a9f8-4824-42c8-98bf-c257d2b4bbc2.svg"></embed>
+          <figure><embed src="https://wakatime.com/share/@ndufreche/9396a66a-8799-4cb4-8663-3ca58b51d10c.svg"></embed></figure>
         </ResponsiveEmbed>
       </Col>
     </Row>
