2b088fd4912e130b1f00508f21f16700e11e3877
Nicolas Dufreche <n.dufreche@gmail.com>
2019-09-19 13:24:04 +0200

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -11,19 +11,19 @@ const Footer = () => (
       <Row className="social-icons">
         {socials.map((social, key) => (<SocialBlock key={key} {...social} />))}
 
-        <Col xsHidden sm={4} smOffset={2} md={3} mdOffset={3} lg={2} lgOffset={4}>
+        <Col xsHidden sm={4} smOffset={2} md={3} mdOffset={3} lg={3} lgOffset={3}>
           <Row className="social-icons">
             {
               [
                 {
-                  url: 'https://travis-ci.org/ndufreche/the.12.gy',
-                  src: 'https://travis-ci.org/ndufreche/the.12.gy.svg?branch=master',
+                  url: 'https://travis-ci.org/ndious/the.12.gy',
+                  src: 'https://travis-ci.org/ndious/the.12.gy.svg?branch=master',
                   alt: 'tarvis-ci build status badge'
                 },
                 {
-                  url: 'https://codeclimate.com/github/ndufreche/the.12.gy',
-                  src: 'https://codeclimate.com/github/ndufreche/the.12.gy/badges/gpa.svg',
-                  alt: 'tcode climate badge'
+                  url: 'https://codeclimate.com/github/ndious/the.12.gy',
+                  src: 'https://codeclimate.com/github/ndious/the.12.gy/badges/gpa.svg',
+                  alt: 'code climate badge'
                 }
               ].map((build, key) => (<BuildBlock key={key} {...build} />))
             }
